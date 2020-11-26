c8f520c2d474a02d73b2f6a7cc32289a668bdf42
Khoa Tran <velims2011@gmail.com>
2020-11-26 10:44:40 +0700

    fix cors

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -96,8 +96,8 @@ const setToken = async (res, user) => {
 
   const cookieOptions = {
     maxAge: process.env.REFRESH_TOKEN_LIFE,
-    sameSite: "none",
-    secure: true,
+    // sameSite: "none",
+    // secure: true,
   };
 
   res.cookie("refreshToken", refreshToken, {
--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.static("./public"));
 
 app.use(
   cors({
-    origin: "*",
+    origin: true,
     credentials: true,
   })
 );
